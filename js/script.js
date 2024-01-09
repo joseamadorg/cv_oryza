6777f2bd4cb9356106884445ef5d6f0175b7f812
amador_jose <amadorjose@gmail.com>
2024-01-09 17:27:21 -0500

    2024: ajustes para pantalla de carga.

--- js/script.js
+++ js/script.js
@@ -67,3 +67,8 @@ function toggleHeroDerTop() {
 
   
 
+  window.addEventListener('load', function() {
+    var pantallaCarga = document.getElementById('pantalla-carga');
+    pantallaCarga.style.display = 'none';
+  });
+  
